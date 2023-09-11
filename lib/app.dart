3fb2bc4a2ae66517721ebd4979be80b19f18c3e5
tuantran22012001 <tuan.tran@inbox.xyz>
2023-09-11 21:48:11 +0700

    implement cubit to counter

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bloc_flutter/counter/counter.dart';
 
-
 class CounterApp extends MaterialApp {
-
   const CounterApp({super.key}) : super(home: const CounterPage());
 }
--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc_flutter/counter/counter.dart';
 
-
 class CounterPage extends StatelessWidget {
-  /// {@macro counter_page}
   const CounterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("counter Page");
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const CounterView(),
+    );
   }
 }
