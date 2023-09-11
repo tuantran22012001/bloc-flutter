109c40160ff1986c2a390e8806583ce9026810f4
tuantran22012001 <tuan.tran@inbox.xyz>
2023-09-11 21:48:11 +0700

    set up structure for counter app

---
+++ lib/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:bloc_flutter/counter/counter.dart';
+
+
+class CounterApp extends MaterialApp {
+
+  const CounterApp({super.key}) : super(home: const CounterPage());
+}
---
+++ lib/counter/view/counter_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc_flutter/counter/counter.dart';
+
+
+class CounterPage extends StatelessWidget {
+  /// {@macro counter_page}
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("counter Page");
+  }
+}
